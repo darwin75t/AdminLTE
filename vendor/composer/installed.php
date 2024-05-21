cb07910986ea2eb65faa8886dde3dd05de75e39f
Darwin Torres <21infopro@gmail.com>
2024-05-20 21:08:05 -0400

    Merge branch 'master' of https://github.com/ColorlibHQ/AdminLTE

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'almasaeed2010/adminlte',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a8620a02bb2b2edba074a8b86e8cc5514d355b51',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'almasaeed2010/adminlte' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a8620a02bb2b2edba074a8b86e8cc5514d355b51',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
